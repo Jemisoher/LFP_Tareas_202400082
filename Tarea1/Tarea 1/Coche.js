0a66c91674d33f087f62ff761a69e52ca36ef322
Jemisoher01 <4003847360101@ingenieria.usac.edu.gt>
2025-08-04 20:27:48 -0600

    Tarea 1

---
+++ Tarea1/Tarea 1/Coche.js
@@ -0,0 +1,34 @@
+class Coche {
+
+  #marca;
+  #modelo;
+  #anio;
+
+
+  constructor(marca, modelo, anio) {
+    this.#marca = marca;
+    this.#modelo = modelo;
+    this.#anio = anio;
+  }
+
+
+  obtenerDescripcion() {
+    return `Coche: ${this.#marca} ${this.#modelo} (${this.#anio})`;
+  }
+
+
+  encender = () => {
+    console.log(`${this.#marca} ${this.#modelo} está encendido.`);
+  };
+
+}
+// Se crean dos instancias
+const coche1 = new Coche("Tesla", "Cybertruck", 2024);
+const coche2 = new Coche("Toyota", "Toyota Camry", 2025);
+
+// Aquí se prueban los métodos
+console.log(coche1.obtenerDescripcion());
+coche1.encender();
+
+console.log(coche2.obtenerDescripcion());
+coche2.encender();
